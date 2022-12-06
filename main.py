b6f18e232e143c8c0ffedf89012698e8b8008f2c
Liya Darvis <57328715+LIya-Darvis@users.noreply.github.com>
2022-12-06 17:58:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
 import pygame
-
 # вызов библиотеки
 pygame.init()
+# создание окна
 screen = pygame.display.set_mode((800, 600))
-
+# добавляем название
 pygame.display.set_caption("Space Stalkers")
-
-icon = pygame.image.load("ufo.png")
+# добавляем иконку
+icon = pygame.image.load("aircraft.png")
 pygame.display.set_icon(icon)
-
-playerImg = pygame.image.load("spaceship.png")
-playerX = 370
+# параметры отображения персонажа
+playerImg = pygame.image.load("taco.png")
+playerX = 340
 playerY = 470
+playerX_change = 0
+playerY_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+# функция отображения персонажа в окне
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
-running = True
+# начало работы программы
+running = True  # флаг для последующего закрытия
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    for event in pygame.event.get():  # определяем тип события
+        if event.type == pygame.QUIT:  # тип события: закрытие окна
+            running = False  # аналогично команде break
+        if event.type == pygame.KEYDOWN:  # тип события: нажатие кнопки
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
 
-    player()
-    pygame.display.update()
+            if event.key == pygame.K_UP:
+                playerY_change = -0.1
+            if event.key == pygame.K_DOWN:
+                playerY_change = 0.1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT or pygame.K_UP or pygame.K_DOWN:
+                playerX_change = 0
+                playerY_change = 0
+
+    playerX += playerX_change
+    playerY += playerY_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    if playerY <= 0:
+        playerY = 0
+    elif playerY >= 536:
+        playerY = 536
+
+    player(playerX, playerY)
+    pygame.display.update()  # постоянное обновление окна
