2248beb7e566a0a6501ace447cc160b23e65dfc8
Liya Darvis <57328715+LIya-Darvis@users.noreply.github.com>
2022-12-18 21:54:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import pygame
 
-# вызов библиотеки
 pygame.init()
 
-# создание окна
-screen = pygame.display.set_mode((800, 600))
+display_width = 800
+display_height = 600
 
-# название окна
-pygame.display.set_caption("Space Breakers")
-
-# картинка должна быть в той же папке, что и файл main.py
-icon = pygame.image.load("blackhole.png")
-pygame.display.set_icon(icon)
+display = pygame.display.set_mode((display_width, display_height))
 
 # игрок
 playerImg = pygame.image.load("spaceship.png")
 playerX = 340
 playerY = 470
 playerX_change = 0
-playerY_change = 0
 
 # пули
 bulletImg = pygame.image.load('bullet.png')
@@ -28,63 +21,57 @@ bulletX_change = 0
 bulletY_change = 10
 bullet_state = "ready"
 
-# функция создания игрока на заданных координатах
-def player(x, y):
-    screen.blit(playerImg, (x, y))
-
 def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
-    screen.blit(bulletImg, (x + 16, y + 10))
+    display.blit(bulletImg, (x + 16, y + 10))
 
-# начало работы программы
-running = True  # создание флага
-while running:
-    for event in pygame.event.get():  # .event для любых событий pygame
-        if event.type == pygame.QUIT:  # тип события - выход из программы
-            running = False  # аналогично команде break
+def player(x, y):
+    display.blit(playerImg, (x, y))
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -0.1
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 0.1
-            if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
-                    bulletX = playerX
-                    bulletY = playerY
-                    fire_bullet(bulletX, bulletY)
+def run_game(playerX, playerY, playerX_change, bulletX, bulletY, bullet_state):
+    game = True
 
-            if event.key == pygame.K_UP:
-                playerY_change = -0.1
-            if event.key == pygame.K_DOWN:
-                playerY_change = 0.1
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                playerX_change = 0
-                playerY_change = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -0.1
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 0.1
+                if event.key == pygame.K_SPACE:
+                    if bullet_state == "ready":
+                        bulletX = playerX
+                        bulletY = playerY
+                        fire_bullet(bulletX, bulletY)
 
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:
-        playerX = 736
-
-    if playerY <= 0:
-        playerY = 0
-    elif playerY >= 536:
-        playerY = 536
-
-    if bulletY <= 0:
-        bulletY = 480
-        bullet_state = "ready"
-
-    if bullet_state is "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
 
 
-    playerX += playerX_change
-    playerY += playerY_change
-    player(playerX, playerY)
-    pygame.display.update()  # постоянное обновление окна
+        display.fill((5, 5, 10))
+
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
+
+        if bulletY <= 0:
+            bulletY = 480
+            bullet_state = "ready"
+
+        if bullet_state == "fire":
+            fire_bullet(bulletX, bulletY)
+            bulletY -= bulletY_change
+
+        playerX += playerX_change
+
+        player(playerX, playerY)
+        pygame.display.update()
+
+run_game(playerX, playerY, playerX_change, bulletX, bulletY, bullet_state)
